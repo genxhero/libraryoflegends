c81ab64de0ddf51c695a997c3000c192187c1073
Aaron Goddard <aaronbear@gmail.com>
2018-12-04 18:44:19 -0800

    Create bidirectional associations between chars and users

--- schema.js
+++ schema.js
@@ -4,6 +4,7 @@ const {
    GraphQLObjectType,
    GraphQLString,
    GraphQLInt,
+   GraphQLList,
    GraphQLSchema
 } = graphql;
 
@@ -11,23 +12,35 @@ const {
 
 const UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
+  fields:  () => ({
     id: { type: GraphQLString },
     username: { type: GraphQLString },
-    email: { type: GraphQLString}
-  }
+    email: { type: GraphQLString},
+    characters: {
+        type: new GraphQLList(CharType),
+        resolve(parentValue, args){
+            return axios.get(`http://localhost:3000/users/${parentValue.id}/characters`).then(res => res.data);
+        }
+    }
+  })
 });
 
 const CharType = new GraphQLObjectType({
    name: "Character",
-   fields: {
+   fields: () => ({
      id: { type: GraphQLString },
-     userId: { type: GraphQLString },
      firstName: { type: GraphQLString },
      lastName: { type: GraphQLString },
      class: { type: GraphQLString },
-     level: {type: GraphQLInt}
-   }
+     level: {type: GraphQLInt},
+     user: { 
+           type: UserType,
+           resolve(parentValue, args){
+              return axios.get(`http://localhost:3000/users/${parentValue.userId}`)
+              .then(res => res.data);
+           } 
+        }
+   })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -38,8 +51,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString }},
             resolve(parentValue, args){
                 return axios.get(`http://localhost:3000/users/${args.id}`).then(res => res.data);
-                //result is nested inside of "data". Graphql doesn't know this. That is why we need to grab
-                //res.data
+//result is nested inside of "data". Graphql doesn't know this. That is why we need to grab res.data
             }
         },
         character: {
