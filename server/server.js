ce4cdf0218c63df9f972b1587d64a752127d6084
Aaron Goddard <aaronbear@gmail.com>
2019-01-05 13:15:31 -0800

    Restart auth after big screwup

--- server/server.js
+++ server/server.js
@@ -3,22 +3,29 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const schema = require('./schema/schema');
+const passport = require("passport");
+const db = require('../config/keys').mongoURI;
+require('../config/passport');
 
 const app = express();
 
-const MONGO_URI =
-  "mongodb://FierySwagger:butter469@ds151840.mlab.com:51840/libraryoflegends";
-if (!MONGO_URI){
-    throw new Error('Invalid URI');
-}
+// const MONGO_URI =
+//   "mongodb://FierySwagger:butter469@ds151840.mlab.com:51840/libraryoflegends";
+// if (!MONGO_URI){
+//     throw new Error('Invalid URI');
+// }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+// mongoose.connect(MONGO_URI);
+mongoose.connect(db);
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+
 app.use('/graphql', expressGraphQL({
     schema,
     graphiql: true
