f93cafb536b22fbc05cc5e62975a2f867c3935b9
Aaron Goddard <aaronbear@gmail.com>
2019-07-30 09:27:07 -0700

    trynna fix this

--- client/components/register.jsx
+++ client/components/register.jsx
@@ -1,15 +1,28 @@
 import SIGNUP_MUTATION from "./session_mutations";
 import React, { Component } from 'react'
+import {graphql} from 'react-apollo';
 
 class Register extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            username: "",
+            password: "",
+            email: "",
+            invalidPass: null
+        }
         this.saveUser = this.saveUser.bind(this);
     }
 
     saveUser() {
-        console.log("Saving user..but not really")
+        this.props.mutate({
+            variables: {
+                email: this.state.email,
+                username: this.state.username,
+                password: this.state.password
+                },
+          }).then( hashHistory.push('/'));
     }
 
     render() {
@@ -17,11 +30,13 @@ class Register extends Component {
             <div>
                <form onSubmit={this.saveUser}>
                    <input type="text" />
+                   <input type="text"/>
                    <input type="text"/> 
-
                    <input type="submit"></input>
                </form>
             </div>
-        )
+        );
     }
 }
+
+export default graphql(SIGNUP_MUTATION)(Register);
