fd4417e1737d5c7da5a6033f11b10332fad4033a
genxhero <genxhero@users.noreply.github.com>
2019-08-14 20:42:53 -0700

    restyle image pane

--- client/components/image_pane.jsx
+++ client/components/image_pane.jsx
@@ -26,13 +26,13 @@ class ImagePane extends Component {
         const file = e.target.files[0];
         console.log(file)
         if (file.size > 500000) {
-          this.setState({fileTooBig: true})
+          this.setState({fileTooBig: true, image: null, photoURL: null})
           return false;
        }
 
        if (!VALID_FILETYPES[file.type]){
          // Extra security in case the html accept gets bypassed.
-         this.setState({invalidType: true})
+         this.setState({invalidType: true, image: null, photoURL: null})
          return false
        }
 
@@ -41,28 +41,30 @@ class ImagePane extends Component {
           this.setState({ image: file, photoURL: fileReader.result, fileTooBig: false, invalidType: false});
         }
         fileReader.readAsDataURL(file);
-
-
       }
 
     render () {
         return (
           <div className="char-creation-pane"> 
-            <h1>Please Select a Picture for Your Character</h1>
+            <h1>Image</h1>
             <form className="char-form" onSubmit={this.passTheProps}>  
                 <div className="image-preview">
-                {this.state.photoURL ? <img src={this.state.photoURL}/> : <div className="image-placeholder"><span>Image Preview</span></div>}
+                {this.state.photoURL ? <img src={this.state.photoURL}/> : <div className="image-placeholder"><span>Image Preview </span></div>}
                 </div> 
-                <input className="char-form-image"
-                  type="file"  
-                  onChange={this.handleFileChange} 
-                  accept="image/png, image/jpeg, image/gif, image/bmp, image/jpg"
-                  />
+                  <div className="char-form-row">
+                    <input className="char-form-image"
+                    type="file"  
+                    onChange={this.handleFileChange} 
+                    accept="image/png, image/jpeg, image/gif, image/bmp, image/jpg"
+                    />
+                  <input type="submit" className="submit" value="NEXT" disabled={!this.state.image} />
+                </div>
+               
+
                 <ul className="error-zone">
                   {this.state.fileTooBig && <li>File Size must be 500 kB or less</li>}
                   {this.state.invalidType && <li>Png, Jpeg, Gif, and Bmp only</li>}
                 </ul>
-                <input type="submit" className="submit" value="NEXT" disabled={!this.state.image} />
             </form>
           </div>
         );
