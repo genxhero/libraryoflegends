a1c644647a24395d5007d055d650d8b57de1ae48
Aaron Goddard <genxhero@users.noreply.github.com>
2019-09-04 19:35:18 -0700

    rudimentary websocket code

--- client/index.js
+++ client/index.js
@@ -2,6 +2,10 @@ import './public/index.css';
 import { split } from 'apollo-link'
 import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from "react-router";
@@ -15,18 +19,62 @@ import Register from './components/register';
 import Login from './components/login';
 import UserPage from './components/user_page';
 
-const networkInterface = createNetworkInterface({
-  uri: "/graphql",
-  opts: {
-    credentials: 'same-origin'
-  }
-});
+
+//Old
+// const networkInterface = createNetworkInterface({
+//   uri: "/graphql",
+//   opts: {
+//     credentials: 'same-origin'
+//   }
+// });
+
+// const client = new ApolloClient({
+//   networkInterface,
+//   dataIdFromObject: o => o.id
+// });
+
+//New
+const httpLink = createHttpLink({
+  // uri: 'http://localhost:4000',
+  uri: "/graphql"
+  //   opts: {
+//     credentials: 'same-origin'
+//   }
+})
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem(AUTH_TOKEN)
+//   return {
+//     headers: {
+//       headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   }
+// })
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      // authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink,
+)
 
 const client = new ApolloClient({
-  networkInterface,
-  dataIdFromObject: o => o.id
-});
-//COnfiguration objects
+  link,
+  cache: new InMemoryCache(),
+})
 
 const Root = () => {
     return (
