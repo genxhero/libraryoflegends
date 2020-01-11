7e7a486c1f6be9aabfdbd4da2e03148b9fceaa09
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-11 12:55:45 -0800

    add toggle component and mutations

--- client/components/cool_toggle.jsx
+++ client/components/cool_toggle.jsx
@@ -1,14 +1,41 @@
 import React, {useState} from 'react';
 import {Mutation} from 'react-apollo';
+import Toggle from 'react-toggle'
 import toggleCool from '..mutations/toggleCool';
 import currentUser from '../queries/current_user';
 
+/**
+ * Expected props
+ * cool, Boolean
+ */
 const CoolToggle = (props) => {
     debugger;
+    const [on, toggle] = useState(props.cool)
     return (
         <div className="cool-switch">
-            <Mutation>
+            <Mutation
+                mutation={toggleCool}
+                refetchQueries={[{ query: currentUser }]}
+            >
+                <Toggle
+                    id="cool-state"
+                    defaultChecked={on}
+                    onChange={
+                        event => {
+                            event.preventDefault();
+                            toggleCool({
+                                variables: {
+                                    id: this.props.id
+                                }
+                            }).then(res => {
+                                debugger;
+                                toggle(res.cool)
+                            })
+                        }
+                    }
+                 />
 
+                <label htmlFor='cool-state'>Adjacent label tag</label>
             </Mutation>
         </div>
     )
