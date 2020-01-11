0c31611c23832397dc297be760d2552c0a107bd6
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-11 12:36:15 -0800

    start the mutation component. cant test until i get some interwebs

---
+++ client/components/cool_toggle.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {Mutation} from 'react-apollo';
+import toggleCool from '..mutations/toggleCool';
+import currentUser from '../queries/current_user';
+
+const CoolToggle = (props) => {
+    debugger;
+    return (
+        <div className="cool-switch">
+            <Mutation>
+
+            </Mutation>
+        </div>
+    )
+
+}
+
+export default CoolToggle;
