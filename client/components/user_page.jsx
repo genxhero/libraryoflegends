873f59753c2c5c804c213362ad3ed7e2551c67b0
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-13 15:43:35 -0800

    next step, fix user page

--- client/components/user_page.jsx
+++ client/components/user_page.jsx
@@ -54,9 +54,11 @@ class UserPage extends Component {
   
   render() {
       if (!this.props.data.user || this.props.data.loading) {
-          return <h1>Loading...</h1>
+          return <h1>Loading user home page...</h1>
       }
       const user = this.props.data.user
+      debugger;
+      console.log(user);
       return (
           <div className="user-profile-page">
             <div className="user-page-top">
