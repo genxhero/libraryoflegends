7a6b7b4adceb288de8a9ff9e0c4460da834bbfbd
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-14 12:54:18 -0800

    backup webpack

--- placeholder.js
+++ placeholder.js
@@ -1,4 +1,6 @@
-const styuff = {"babel-core": "^6.22.1",
-"babel-polyfill": "^6.26.0",
-"babel-preset-env": "^1.1.8",
-"babel-preset-react": "^6.22.0"}
+// const styuff = {"babel-core": "^6.22.1",
+// "babel-polyfill": "^6.26.0",
+// "babel-preset-env": "^1.1.8",
+// "babel-preset-react": "^6.22.0"
+// "webpack": "4.41.5",
+// "webpack-dev-middleware": "3.7.2"}
