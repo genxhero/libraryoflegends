bbbaead5ed7590cf9421866223dbfb3476098048
genxhero <genxhero@users.noreply.github.com>
2020-03-20 23:12:46 -0700

    revert unnecesary refactor

--- client/components/CharShow/CharData.jsx
+++ client/components/CharShow/CharData.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 const CharData = (props) => {
     return (
-        <div>
-            
+        <div style={{ "display": "flex", "flexDirection": "row" }}>
+            <div className="char-show-fieldnames" style={{ "display": "flex", "flex-direction": "column", "align-items": "flex-end" }}>
+                <span className="char-vital-fieldname">Player:{' '}</span>
+                <span className="char-vital-fieldname">Class:{' '}</span>
+                <span className="char-vital-fieldname">Level:{' '}</span>
+            </div>
+
+            <div className="char-show-data" style={{ "display": "flex", "flex-direction": "column", "justify-content": "flex-start" }}>
+                <span className="char-vital"><Link to={`/users/${char.user.username}`}>{char.user.username}</Link> </span>
+                <span className="be-capitalized char-vital">{char.class}</span>
+                <span className="char-vital">{char.level}</span>
+            </div>
         </div>
     )
 }
