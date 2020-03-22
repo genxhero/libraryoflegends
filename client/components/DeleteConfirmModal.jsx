0a3e88c52108bfb20392833d490ba38d699d1f9c
genxhero <genxhero@users.noreply.github.com>
2020-03-22 11:27:53 -0700

    change char to props.char

--- client/components/DeleteConfirmModal.jsx
+++ client/components/DeleteConfirmModal.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const DeleteConfirmModal = prop => {
+   
     return (
         <div className="confirmation-modal">
             <div className="confirmation-dialog">
                 <h1>Confirm Deletion</h1>
-                <h3>Are you sure you want to delete {char.firstName} {char.lastName}???</h3>
+                <h3>Are you sure you want to delete {props.char.firstName} {props.char.lastName}???</h3>
                 <div style={{ "display": "flex", "justifyContent": "center" }}>
                     <button className="confirm-btn" onClick={props.delete} value={char.id}>Yes</button>
                     <button className="confirm-btn" onClick={props.closeConfirmationModal}>No</button>
