db8d73d17d7b069d14f247dea873cdb5fe4b68fe
genxhero <genxhero@users.noreply.github.com>
2020-03-24 21:05:08 -0700

    back up old files

---
+++ backup/index.js
@@ -0,0 +1,7 @@
+const app = require('./server/server');
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log('Listening');
+});
+
+    // "react": "15.4.2",
---
+++ backup/webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ["babel-polyfill", "./client/index.js"],
+  output: {
+    path: "/",
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+    },
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.jsx?$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ["style-loader", "css-loader", "sass-loader"],
+        // test: /\.css$/
+        test: /\.s[ac]ss$/i,
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }    
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "client/index.html"
+    })
+  ]
+};
